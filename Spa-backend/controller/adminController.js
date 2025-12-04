b1b4f177bc63887722623a8843ada7da4a3efe57
Blink-in <emmanuelebirim27@gmail.com>
2025-12-04 12:13:38 +0100

    bcrypt fix

--- Spa-backend/controller/adminController.js
+++ Spa-backend/controller/adminController.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcryptjs")
 const generateToken = require("../utils/jwt.js")
 const Admin = require('../module/adminModule.js');
 
